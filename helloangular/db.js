1bf75d2d35eee78c819849b8ae128012fb297cee
Leonardo Silveira <sombriks@gmail.com>
2016-12-06 02:48:45 -0300

    alteração no submódulo

--- helloangular/db.js
+++ helloangular/db.js
@@ -1,3 +1,4 @@
+// db.js // testando alteração no submódulo
 const knexfile = require("./knexfile.js");
 const env = process.env.NODE_ENV || "development";
 const knex = require("knex")(knexfile[env]);
